3300c40492c531f1ff16359636565edf6ec0e254
fardyzg <117874309+fardyzg@users.noreply.github.com>
2023-09-20 12:53:31 -0500

    listo

--- views/compras.php
+++ views/compras.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="icon" href="../public/images/logo_sin_fondo.png" type="image/x-icon">
+  <link rel="icon" href="../public/images/TAMI LOGO transparente 2.png" type="image/x-icon">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Compras | TAMI</title>
   <link rel="stylesheet" type="text/css" href="../public/css/menu.css">
+  <link rel="stylesheet" type="text/css" href="../public/css/compras.css">
   <link rel="stylesheet" type="text/css" href="../public/css/footer.css">
-  <link rel="stylesheet" type="text/css" href="../public/css/">
 </head>
 <body>
   <?php include_once "menu.php";?>
   <main class="main__compras">
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    
   </main>
   <?php include_once "footer.php"; ?>
   <script type="application/javascript" src="../public/js/main.js" async></script>
