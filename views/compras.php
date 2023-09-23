ac9b1996e4c498b15d513cdb49456fa7ed749dfd
programadordc <137285862+programadordc@users.noreply.github.com>
2023-09-23 08:11:20 -0500

    DANIEL ACTUALIZACION_23_09_2023
    Mejora del carrusel y la pagina pago.

--- views/compras.php
+++ views/compras.php
@@ -9,6 +9,142 @@
   <link rel="stylesheet" type="text/css" href="../public/css/menu.css">
   <link rel="stylesheet" type="text/css" href="../public/css/compras.css">
   <link rel="stylesheet" type="text/css" href="../public/css/footer.css">
+</head>
+<style>
+  /* Estilos para el contenedor del carrito */
+  .carrito-container {
+    display: none;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(43, 43, 43, 0.5);
+    /* Fondo semitransparente para el modal */
+    z-index: 999;
+    /* Coloca el carrito por encima de todos los elementos */
+  }
+
+  /* Estilos para el carrito */
+  .carrito {
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+    width: 400px;
+    padding: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: left;
+    color: #2B2B2B;
+  }
+
+  .carrito h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+    color: #2B2B2B;
+  }
+
+  .carrito ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .carrito li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 0;
+  }
+
+  .carrito li:last-child {
+    border-top: 1px solid #ccc;
+  }
+
+  .carrito button {
+    background-color: #FF4444;
+    border: none;
+    color: white;
+    padding: 5px 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  .carrito button:hover {
+    background-color: #FF8282;
+  }
+
+  /* Estilos para el icono de carrito */
+  .cart-icon {
+    font-size: 30px;
+    cursor: pointer;
+    position: relative;
+    display: flex;
+    align-items: center;
+    color: #2B2B2B;
+  }
+
+  /* Estilos para la cantidad de productos en el botón de carrito */
+  .cart-quantity {
+    background-color: #ff4444;
+    color: white;
+    border-radius: 50%;
+    padding: 4px 8px;
+    font-size: 14px;
+    position: absolute;
+    /* Posición absoluta para posicionar con top y right */
+    top: -5px;
+    margin-left: 25px;
+    /* Ajusta la posición vertical */
+    /* Ajusta la posición horizontal */
+  }
+
+  /* Estilos para el botón de cerrar el carrito */
+  .close-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 24px;
+    color: #666;
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+  }
+
+  .close-btn:hover {
+    color: #2B2B2B;
+  }
+
+  /* Estilos para los botones del carrito */
+  .carrito-buttons {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+  }
+
+  .carrito-buttons button {
+    flex: 1;
+    margin: 0 5px;
+    /* Espacio entre los botones */
+    width: 50%;
+    /* Divide el espacio disponible entre los dos botones */
+    background-color: #2B2B2B;
+    color: #fff;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  .carrito-buttons button:hover {
+    background-color: #949494;
+  }
+</style>
+
 
 <body>
   <?php include_once "menu.php"; ?>
@@ -28,8 +164,7 @@
               <h2 class="precio">S/.2500</h2>
             </div>
             <div class="contenedor_btn">
-              <button class="button">Añadir</button>
-
+              <button class="button add-to-cart">Añadir</button>
               <button class="button">Cantidad</button>
             </div>
           </div>
@@ -45,7 +180,7 @@
               <h2 class="precio">S/.1500</h2>
             </div>
             <div class="contenedor_btn">
-              <button class=" button">Añadir</button>
+              <button class="button add-to-cart">Añadir</button>
               <button class="button">Cantidad</button>
             </div>
           </div>
@@ -61,8 +196,8 @@
               <h2 class="precio">S/.3500</h2>
             </div>
             <div class="contenedor_btn">
-              <button class=" button">Añadir</button>
-              <button class=" button">Cantidad</button>
+              <button class="button add-to-cart">Añadir</button>
+              <button class="button">Cantidad</button>
             </div>
           </div>
         </div>
@@ -70,11 +205,150 @@
       </div>
       <button id="nextBtn" class="carousel-btn">&#9654;</button>
     </div>
+    <div class="cart-icon" id="mostrarCarrito">
+      &#128722;
+      <span class="cart-quantity" id="carrito-cantidad">0</span>
+    </div>
+
+
   </main>
+
+  <div class="carrito-container">
+    <div class="carrito">
+      <h2>Carrito de Compras</h2>
+      <ul id="carrito-lista"></ul>
+      <p>Total: <span id="carrito-total">S/.0.00</span></p>
+      <div class="carrito-buttons">
+        <button id="vaciar-carrito" class="button">Vaciar Carrito</button>
+        <button id="pagar" class="button">Pagar</button>
+      </div>
+    </div>
+  </div>
 
   <?php include_once "footer.php"; ?>
   <script type="application/javascript" src="../public/js/main.js" async></script>
   <script src="../public/js/compras.js"></script>
 </body>
+<script>
+  // JavaScript para mostrar y ocultar el carrito
+  const mostrarCarritoBtn = document.getElementById('mostrarCarrito');
+  const carritoContainer = document.querySelector('.carrito-container');
+
+  mostrarCarritoBtn.addEventListener('click', () => {
+    carritoContainer.style.display = 'block';
+  });
+
+  carritoContainer.addEventListener('click', (event) => {
+    if (event.target === carritoContainer) {
+      carritoContainer.style.display = 'none';
+    }
+  });
+
+  // Obtener todos los botones "Añadir" del carrusel
+  const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+  // Elementos del carrito
+  const carritoLista = document.getElementById('carrito-lista');
+  const carritoTotal = document.getElementById('carrito-total');
+  const carritoCantidad = document.getElementById('carrito-cantidad');
+
+  // Inicializar el carrito como un arreglo vacío
+  let carrito = [];
+
+  // Función para actualizar el carrito y la vista del carrito
+  function actualizarCarrito() {
+    // Limpiar la vista del carrito
+    carritoLista.innerHTML = '';
+
+    // Calcular el total
+    let total = 0;
+
+    // Recorrer el carrito y agregar cada producto al carrito
+    carrito.forEach((producto, index) => {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <span>${producto.nombre}</span>
+        <span>S/.${producto.precio.toFixed(2)}</span>
+        <button class="eliminar" data-index="${index}">Eliminar</button>
+      `;
+      carritoLista.appendChild(listItem);
+
+      // Sumar el precio del producto al total
+      total += producto.precio;
+    });
+
+    // Actualizar la cantidad en el botón de carrito
+    carritoCantidad.textContent = carrito.length;
+
+    // Actualizar el total en la vista
+    carritoTotal.textContent = `Total: S/.${total.toFixed(2)}`;
+  }
+
+  // Manejar el clic en los botones "Añadir"
+  addToCartButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      // Obtener los datos del producto desde el carrusel
+      const nombre = document.querySelectorAll('.nombre')[index].textContent;
+      const precioTexto = document.querySelectorAll('.precio')[index].textContent;
+      const precio = parseFloat(precioTexto.replace('S/.', '').replace(',', '')); // Convertir el precio a número
+
+      // Agregar el producto al carrito
+      carrito.push({
+        nombre,
+        precio
+      });
+
+      // Actualizar el carrito y la vista del carrito
+      actualizarCarrito();
+    });
+  });
+
+  // Manejar la eliminación de productos del carrito
+  carritoLista.addEventListener('click', (event) => {
+    if (event.target.classList.contains('eliminar')) {
+      const index = event.target.getAttribute('data-index');
+      carrito.splice(index, 1);
+      actualizarCarrito();
+    }
+  });
+
+
+
+  // Botón para vaciar el carrito
+  const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+  vaciarCarritoBtn.addEventListener('click', () => {
+    carrito = []; // Vaciar el carrito
+    actualizarCarrito(); // Actualizar la vista del carrito
+  });
+
+  // Botón para pagar (puedes agregar la funcionalidad deseada aquí)
+  // Botón para pagar (puedes agregar la funcionalidad deseada aquí)
+  const pagarBtn = document.getElementById('pagar');
+
+  pagarBtn.addEventListener('click', () => {
+    // Calcular el total del carrito
+    const total = carrito.reduce((acc, producto) => acc + producto.precio, 0);
+
+    // Construir un objeto URLSearchParams con los datos del carrito y el total
+    const params = new URLSearchParams();
+    params.append('total', total);
+
+    carrito.forEach((producto, index) => {
+      params.append(`nombre[${index}]`, producto.nombre);
+      params.append(`precio[${index}]`, producto.precio);
+    });
+
+    // Redirigir a la página "pago.php" con los datos del carrito y el total en la URL
+    window.location.href = `pago.php?${params.toString()}`;
+  });
+
+
+
+
+
+  // Inicializar el carrito y la vista del carrito
+  actualizarCarrito();
+</script>
 
 </html>
